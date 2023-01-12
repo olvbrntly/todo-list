dd0c0dafc34f4880c78da31156641d19fd24de13
olive <olivia.brantley9@gmail.com>
2023-01-11 17:08:18 -0800

    hard coded first task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import addTasktoDOM from "./site";
 //puts the whole thing together
 
+addTasktoDOM();
 // event listener for each button on project
 
 // all tasks button
@@ -9,7 +11,12 @@
 // this week button
 
 // starred button
+const starred =document.getElementById('starred');
 
+starred.addEventListener('click',() =>{
+   starred.classList.toggle('starred');
+
+});
 // add project button
 
 // add task button
--- src/site.js
+++ src/site.js
@@ -5,6 +5,60 @@
 // update a tasks content when edited
 
 // make a task appear when added
+// hard coding a fake task first
+
+function addTasktoDOM(){
+    const taskBox = document.getElementById('tasks');
+
+    // make a new task
+    const taskContainter = document.createElement('div');
+    const leftDiv = document.createElement('div');
+    const rightDiv = document.createElement('div');
+    
+    taskContainter.setAttribute('class','task');
+    rightDiv.setAttribute('class','task-rightDiv');
+    leftDiv.setAttribute('class','task-leftDiv');
+
+
+    const title = document.createElement('div');
+    title.textContent = "My First Task";
+
+    const description =document.createElement('div');
+    description.textContent = "this is the description"
+
+    const starred = document.createElement('span');
+    const dueDate = document.createElement('div');
+    const more = document.createElement('span');
+
+    title.setAttribute('class','task-title');
+
+    starred.setAttribute('class', 'material-icons');
+    starred.setAttribute('id','starred');
+
+    more.setAttribute('class', 'material-icons');
+
+    starred.textContent = 'star'
+    dueDate.textContent = 'due!'
+    more.textContent = 'more_vert';
+    
+    leftDiv.appendChild(title);
+    leftDiv.appendChild(description);
+
+    rightDiv.appendChild(starred);
+    rightDiv.appendChild(dueDate);
+    rightDiv.appendChild(more);
+
+    taskContainter.appendChild(leftDiv);
+    taskContainter.appendChild(rightDiv);
+
+    taskBox.appendChild(taskContainter);
+
+
+    console.log('works')
+    
+};
+
+export default addTasktoDOM;
 
 // move a task to a project
 
