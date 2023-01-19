215a1b45fde5ebb4defb1baeb9cef59731912a7d
olivia <olivia.brantley9@gmail.com>
2023-01-19 13:32:54 -0800

    tasks go to corrent array, 2 links display correct

--- src/DOMEvents.js
+++ src/DOMEvents.js
@@ -1,7 +1,7 @@
  import {createForm, closeForm} from './form';
  import { createNewTask, makeToDo} from './todo';
  import Project from './project';
- import addTasktoDOM from './site';
+ import {addTasktoDOM, clearDOM} from './site';
  
  // adds event listeners to whole document to account for dynamically added elements
 const DOMEvents = () => {
@@ -29,30 +29,51 @@ const events =(e) => {
         e.preventDefault();
         let title = document.getElementById('task-name').value;
         let description = document.getElementById('task-description').value;
-        const newTask = new createNewTask(title, description, false,'today');
+        let star = document.getElementById('task-star').checked;
+        const newTask = new createNewTask(title, description, star,'today');
         allTasks.addTask(newTask);
-        console.log(allTasks.getTasks());
-        console.log(newTask.getTitle());
+        // console.log(allTasks.getTasks());
+        // console.log(newTask.getTitle());
         addTasktoDOM(newTask);
         closeForm();
         
-        
-
         if(newTask.getStar() == true){
-            starredTasks.setTask(newTask);
+            starredTasks.addTask(newTask);
         }
 
         if(newTask.getDate() ==='today'){
-            todaysTasks.setTask(newTask);
+            todaysTasks.addTask(newTask);
         }
      
+        console.log('starred'+ starredTasks.getTasks());
+        console.log(todaysTasks.getTasks());
 
         let btn = document.getElementById('add-tasks');
         btn.disabled = false;
    
-
-      
     }
+
+    if(e.target.id == 'All-Task-Link'){
+        clearDOM();
+        console.log('link');
+        let array = allTasks.getTasks();
+        console.log(array);
+        for(let i = 0; i < array.length; i++){
+            addTasktoDOM();
+        };
+    }
+
+    if(e.target.id == 'Starred-Task-Link'){
+        clearDOM();
+        console.log('link');
+        let array = starredTasks.getTasks();
+        console.log(array);
+        for(let i = 0; i < array.length; i++){
+            addTasktoDOM();
+        };
+    }
+
+
 }
 
  export default DOMEvents
