88d435d7e034238167e33192e8093cdb66a0dc3d
olive <olivia.brantley9@gmail.com>
2023-01-20 16:47:33 -0800

    gave add task form some basic styling

--- src/form.js
+++ src/form.js
@@ -1,7 +1,3 @@
-import createNewTask from './task';
-import makeToDo from './index';
-
-
 function createForm(){
     const taskBox = document.getElementById('tasks');
     //creates form
@@ -12,22 +8,56 @@ function createForm(){
     form.setAttribute('id','form');
 
     //creates title input
+
+    const titleDiv = document.createElement('div');
+    titleDiv.setAttribute('id','titleDiv');
+    titleDiv.setAttribute('class','input-div');
+
+    const titleLabel = document.createElement('label');
+    titleLabel.setAttribute('for', 'task-name');
+    titleLabel.textContent = "Title: "
+
     const title = document.createElement('input');
     title.setAttribute('type','text');
     title.setAttribute('name','task-name');
     title.setAttribute('id','task-name');
 
+    titleDiv.appendChild(titleLabel);
+    titleDiv.appendChild(title);
+
     //creates description input
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.setAttribute('id','descriptionDiv');
+    descriptionDiv.setAttribute('class','input-div');
+
+    const descriptionLabel = document.createElement('label');
+    descriptionLabel.setAttribute('for', 'task-name');
+    descriptionLabel.textContent = "Description: "
+
     const description = document.createElement('input');
     description.setAttribute('type','text');
     description.setAttribute('name','task-description');
     description.setAttribute('id','task-description');
 
+    descriptionDiv.appendChild(descriptionLabel);
+    descriptionDiv.appendChild(description);
+
     //creates date input
+    const dateDiv = document.createElement('div');
+    dateDiv.setAttribute('id','dateDiv');
+    dateDiv.setAttribute('class','input-div');
+
+    const dateLabel = document.createElement('label');
+    dateLabel.setAttribute('for', 'task-name');
+    dateLabel.textContent = "Due Date: "
+
     const date = document.createElement('input');
     date.setAttribute('type','date');
     date.setAttribute('name','task-date');
     date.setAttribute('id','task-date');
+
+    dateDiv.appendChild(dateLabel);
+    dateDiv.appendChild(date);
 
     //creates submit button
     const formSubmit = document.createElement('submit');
@@ -36,10 +66,11 @@ function createForm(){
     formSubmit.textContent = "Submit"
 
     //attaching elements to form
-    form.appendChild(title);
-    form.appendChild(description);
-    form.appendChild(date);
+    form.appendChild(titleDiv);
+    form.appendChild(descriptionDiv);
+    form.appendChild(dateDiv);
     form.appendChild(formSubmit);
+
     taskBox.appendChild(form);
 
 }
