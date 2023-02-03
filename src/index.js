3d9cfbeec4189d3eeef733abba094f939ffd39aa
olive <olivia.brantley9@gmail.com>
2023-02-02 21:49:01 -0800

    removed comments

--- src/index.js
+++ src/index.js
@@ -6,22 +6,3 @@ document.addEventListener('DOMContentLoaded', () =>{
 })
 
 
-//from date fsn-
-//use isToday to check if date is today.
-//if so it goes into date array
-//if so task.today = true - displays in today log
-
-//isThisWeek
-//a revelations
-//delte today and this week tasjsa
-//there is only an all task array.
-//use the functions to get each date  thing -today and this week
-//use that to display when clicking on this week and today links
-//then when the new project is added current project changes to that
-//the task is added to that project as well as all projects
-//now when the task is deleted it only needs to search through its 
-//project and the all task array!!
-//lovew calrity
-
-
-
